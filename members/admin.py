eca10591d518a4cacb539d5df873be4368183338
NiyatiTrivedi926 <nt74166n@pace.edu>
2024-11-22 13:15:40 -0700

    Initial commit

---
+++ members/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Member
+
+from django.contrib import admin
+from .models import Member
+
+# Register your models here.
+
+class MemberAdmin(admin.ModelAdmin):
+  list_display = ("firstname", "lastname", "joined_date",)
+  prepopulated_fields = {"slug": ("firstname", "lastname")}
+  
+admin.site.register(Member, MemberAdmin)
+
